3faf43bd61c2a5c8da08db5aa58cae67cd64770a
Uday Trivedi <trivediu@gmail.com>
2019-07-12 15:22:21 -0700

    Create QuestionBank Class With Data

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -2,16 +2,14 @@
 //  ViewController.swift
 //  Quizzler
 //
-//  Created by Angela Yu on 25/08/2015.
-//  Copyright (c) 2015 London App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     
     //Place your instance variables here
     
+    //initialize a question bank
+    let allQuestions = QuestionBank()
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
